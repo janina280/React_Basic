7e25781fdb3cbe30e1662dff1c7c59e4ba29839e
AMC-3\LENOVO <coceijanina@yahoo.com>
2025-01-20 10:34:15 +0200

    delete post

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,15 +1,29 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
+  const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 2 },
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 3 },
+    { title: "Welcome!", body: "lorem ipsum...", author: "luigi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
   ]);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
   return (
     <div className="home">
-      <div></div>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's Blogs"
+      />
     </div>
   );
 };
